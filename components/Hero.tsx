4d4b0842195b902ae4988bd8ffe1a6efccef0ceb
ModouLaminJagne <jagnemlg@gmai.com>
2024-05-30 01:54:43 +0000

    chore: Add utils/cn.ts for merging class names using clsx and tailwind-merge Chore: Add hero section that containcs the intro to the website.

---
+++ components/Hero.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { Spotlight } from './ui/Spotlight'
+import { TextGenerateEffect } from './ui/TextGenerateEffects';
+import MagicButton from './ui/MagicButton';
+import { FaLocationArrow } from 'react-icons/fa';
+
+export const Hero = () => {
+  return (
+    <div className='pb-20 pt-36' >
+        <div>
+            <Spotlight className='-top-40 -left-10 md:-left-32 
+            md:-top-20 h-screen' fill="white"/>
+            <Spotlight className='top-10 left-full h-[80hv]
+            w-[50vw]' fill="purple"/>
+            <Spotlight className='top-28 left-80h h-[80hv]
+            w-[50vw]' fill="blue"/>
+        </div>
+        <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.02] bg-grid-black/[0.2]
+          flex items-center justify-center absolute top-0 left-0">
+      {/* Radial gradient for the container to give a faded look */}
+          <div className="absolute pointer-events-none inset-0 flex items-center justify-center
+          dark:bg-black-100 bg-white 
+          [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+        </div>
+
+                <div className='flex justify-center relative my-20 z-10'>
+                  <div className='max-w-[89vw] md:max-w-screen-2xl lg:mxw[60vw]
+                   flex flex-col items-center justify-center'>
+                    <h2 className='uppercase tracking-widest text-xs text-center
+                     text-blue-100 max-w-80'>
+                      Dynamic Web Magic with Next.js
+                    </h2>
+                    <TextGenerateEffect
+                      words='Transforming Concepts into Seamless User Experiences'
+                      className='text-[40px] md:text-5xl text-center lg:text-6xl'
+                    />
+
+                    <p>
+                      Hi there! I&apos;m MLJagne, a web developer based in The Gambia.
+                    </p>
+
+                    <a href='#about'>
+                      <MagicButton 
+                        title="Show my work" 
+                        icon = {<FaLocationArrow />} // Icon from react-icons library. className='ml-2'
+                        position='right'
+                      />
+                    </a>
+
+                  </div>
+                </div>
+    </div>
+  )
+}
