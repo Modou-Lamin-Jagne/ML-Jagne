fb41843fe2c2300d20efce84e06748d3887ea173
ModouLaminJagne <jagnemlg@gmai.com>
2024-07-30 20:37:12 +0000

    chore: Update Hero component with correct name and location

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -10,9 +10,9 @@ export const Hero = () => {
         <div>
             <Spotlight className='-top-40 -left-10 md:-left-32 
             md:-top-20 h-screen' fill="white"/>
-            <Spotlight className='top-10 left-full h-[80hv]
+            <Spotlight className='top-10 left-full h-[80vh]
             w-[50vw]' fill="purple"/>
-            <Spotlight className='top-28 left-80 h-[80hv]
+            <Spotlight className='top-28 left-80 h-[80vh]
             w-[50vw]' fill="blue"/>
         </div>
         <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.02] bg-grid-black/[0.2]
