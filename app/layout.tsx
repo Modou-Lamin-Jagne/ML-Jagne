3763f1ba4e3337cd1bda53507d8c78fcf95b5436
Modou-Lamin-Jagne <mj22111009@utg.edu.gm>
2025-06-16 19:34:34 +0000

    refactor: comment out Analytics import and usage in layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./provider";
-import { Analytics } from "@vercel/analytics/next"
+// import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,7 +27,7 @@ export default function RootLayout({
           >
             {children}
           </ThemeProvider>
-        <Analytics />
+        {/* <Analytics /> */}
           </body>
     </html>
   );
